9e09c3536800e629c2d29b351fb287432ee67119
Your Name <you@example.com>
2022-09-10 18:28:36 +0530

    Wfds

---
+++ components/ItemComponent.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+export default function ItemComponent() {
+    const [image, setImage] = useState('https://cdn.shopify.com/s/files/1/0602/9334/9613/files/01-01_600x.jpg');
+    const [iphone, setIphone] = useState("https://cdn.shopify.com/s/files/1/0602/9334/9613/products/Xiaomi-Redmi-Note-9-Pro-Max_Color__Champagne-Gold.png?v=1661601361");
+    const [iphonepic, setIphonepic] = useState("https://cdn.shopify.com/s/files/1/0602/9334/9613/products/Xiaomi-Redmi-Note-9-Pro-Max-back.png?v=1661601360");
+
+    return (
+        <div className='bg-gray-200'>
+            <div className='flex'>
+                <p className='px-2 py-1 mx-1 my-3 bg-blue-300 cursor-pointer' onClick={() => setImage('https://cdn.shopify.com/s/files/1/0602/9334/9613/files/03_802d4a58-d459-40a1-af5f-a3c04178ce94_600x.jpg')}>blue</p>
+                <p className='px-2 py-1 mx-1 my-3 cursor-pointer bg-orange-300' onClick={() => setImage('https://cdn.shopify.com/s/files/1/0602/9334/9613/files/01-01_600x.jpg')}>orange</p>
+                <p className='px-2 py-1 mx-1 my-3 cursor-pointer bg-red-300'
+                    onClick={() => { setIphone('https://cdn.shopify.com/s/files/1/0602/9334/9613/products/iPhone-12-Color__Black.png?v=1662038653'); setIphonepic('https://cdn.shopify.com/s/files/1/0602/9334/9613/products/iPhone-12-back.png?v=1662038647') }}
+                >
+                    Iphone 12
+                </p>
+                <p className='px-2 py-1 mx-1 my-3 cursor-pointer bg-red-600'
+                    onClick={() => { setIphone('https://cdn.shopify.com/s/files/1/0602/9334/9613/products/Xiaomi-Redmi-Note-9-Pro-Max_Color__Champagne-Gold.png?v=1661601361'); setIphonepic('https://cdn.shopify.com/s/files/1/0602/9334/9613/products/Xiaomi-Redmi-Note-9-Pro-Max-back.png?v=1661601360') }}
+                >Redmi</p>
+            </div>
+            <div className='' style={{ backgroundImage: "url(" + iphone + ")", backgroundRepeat: 'no-repeat', color: { iphone } }}>
+                <div class="mask1">
+                    <img src={image} alt="Cinque Terre" style={{ webkitMaskImage: "url(" + iphonepic + ")" }} className='h-[800px]' />
+                </div>
+                <div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ pages/Item/[slug].js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../../components/Footer'
+import ItemComponent from '../../components/ItemComponent'
+import Navbar from '../../components/Navbar'
+
+export default function slug() {
+    return (
+        <div>
+            <Navbar />
+            <ItemComponent />
+            {/* <Footer /> */}
+        </div>
+    )
+}
