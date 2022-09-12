876848361e2e21ac2216c6881c20fc56cb79ff8e
Your Name <you@example.com>
2022-09-12 18:21:39 +0530

    FSd

--- components/ItemComponent.jsx
+++ components/ItemComponent.jsx
@@ -1,12 +1,12 @@
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import React, { useState, useEffect } from 'react'
 
 
 
 export default function ItemComponent() {
-    const router = useRouter()
-    const fileId = router.query.slug
-    console.log(fileId)
+    // const router = useRouter()
+    // const fileId = router.query.slug
+    // console.log(fileId)
 
     const [image, setImage] = useState('https://cdn.shopify.com/s/files/1/0602/9334/9613/files/01-01_600x.jpg');
     const [iphone, setIphone] = useState("");
@@ -16,25 +16,26 @@ export default function ItemComponent() {
     }, []);
 
     return (
-        <div className='bg-gray-200 '>
-            <div className='flex '>
-                <p className='px-2 py-1 mx-1 my-3 bg-blue-300 cursor-pointer' onClick={() => setImage('https://cdn.shopify.com/s/files/1/0602/9334/9613/files/03_802d4a58-d459-40a1-af5f-a3c04178ce94_600x.jpg')}>blue</p>
-                <p className='px-2 py-1 mx-1 my-3 cursor-pointer bg-orange-300' onClick={() => setImage('https://cdn.shopify.com/s/files/1/0602/9334/9613/files/01-01_600x.jpg')}>orange</p>
-                <p className='px-2 py-1 mx-1 my-3 cursor-pointer bg-red-300'
-                    onClick={() => { setIphone('https://cdn.shopify.com/s/files/1/0602/9334/9613/products/iPhone-12-Color__Black.png?v=1662038653'); setIphonepic('https://cdn.shopify.com/s/files/1/0602/9334/9613/products/iPhone-12-back.png?v=1662038647') }}
-                >
-                    Iphone 12
-                </p>
-                <p className='px-2 py-1 mx-1 my-3 cursor-pointer bg-red-600'
-                    onClick={() => { setIphone('https://cdn.shopify.com/s/files/1/0602/9334/9613/products/Xiaomi-Redmi-Note-9-Pro-Max_Color__Champagne-Gold.png?v=1661601361'); setIphonepic('https://cdn.shopify.com/s/files/1/0602/9334/9613/products/Xiaomi-Redmi-Note-9-Pro-Max-back.png?v=1661601360') }}
-                >Redmi</p>
-            </div>
-            <div style={{ backgroundImage: "url(" + iphone + ")", backgroundRepeat: 'no-repeat' }} className='-ml-[43%] md:ml-0'>
+        <div className='bg-gray-600 grid grid-cols-2 '>
+
+            <div style={{ backgroundImage: "url(" + iphone + ")", backgroundRepeat: 'no-repeat' }} className='-ml-[43%] md:ml-0 scale-50 -mt-20 border'>
                 <div className='w-screen/2'  >
                     <img src={image} alt="Cinque Terre" style={{ webkitMaskImage: "url(" + iphonepic + ")", WebkitMaskRepeat: 'no-repeat' }} className=' h-screen ' />
                 </div>
                 <div>
                 </div>
+            </div>
+            <div className=' '>
+                <p className='px-2 py-1 mx-1 my-3 bg-blue-300 cursor-pointer max-w-max' onClick={() => setImage('https://cdn.shopify.com/s/files/1/0602/9334/9613/files/03_802d4a58-d459-40a1-af5f-a3c04178ce94_600x.jpg')}>blue</p>
+                <p className='px-2 py-1 mx-1 my-3 cursor-pointer bg-orange-300 max-w-max' onClick={() => setImage('https://cdn.shopify.com/s/files/1/0602/9334/9613/files/01-01_600x.jpg')}>orange</p>
+                <p className='px-2 py-1 mx-1 my-3 cursor-pointer bg-red-300 max-w-max'
+                    onClick={() => { setIphone('https://cdn.shopify.com/s/files/1/0602/9334/9613/products/iPhone-12-Color__Black.png?v=1662038653'); setIphonepic('https://cdn.shopify.com/s/files/1/0602/9334/9613/products/iPhone-12-back.png?v=1662038647') }}
+                >
+                    Iphone 12
+                </p>
+                <p className='px-2 py-1 mx-1 my-3 cursor-pointer bg-red-600 max-w-max'
+                    onClick={() => { setIphone('https://cdn.shopify.com/s/files/1/0602/9334/9613/products/Xiaomi-Redmi-Note-9-Pro-Max_Color__Champagne-Gold.png?v=1661601361'); setIphonepic('https://cdn.shopify.com/s/files/1/0602/9334/9613/products/Xiaomi-Redmi-Note-9-Pro-Max-back.png?v=1661601360') }}
+                >Redmi</p>
             </div>
         </div>
     )
