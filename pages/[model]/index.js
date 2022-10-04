36fb4ead287fe70858dfe990a6233b6495c62694
Your Name <you@example.com>
2022-10-04 10:06:13 +0530

    bhh

--- pages/[model]/index.js
+++ pages/[model]/index.js
@@ -1,43 +1,43 @@
-import React from 'react'
-// import { useRouter } from 'next/router'
-import Navbar from '../../components/Navbar'
-import { mobile } from '../../data/mobilee'
-import Populated from '../../components/PopulatedItem/Populated'
-import { theme } from '../../data/design'
+// import React from 'react'
+// // import { useRouter } from 'next/router'
+// import Navbar from '../../components/Navbar'
+// import { mobile } from '../../data/mobilee'
+// import Populated from '../../components/PopulatedItem/Populated'
+// import { theme } from '../../data/design'
 
 
 
-function index() {
-    // console.log(theme.Satrang[0].URL)
-    // const { query } = useRouter()
-    // console.log(query.model)
-    // console.log(mobile.Samsung.model['S20 Ultra'].back)
-    return (
-        <>
-            <Navbar />
-            <div className=''>
-                {
-                    Object.keys(theme).map((item, index) => {
-                        return (
+// function index() {
+//     // console.log(theme.Satrang[0].URL)
+//     // const { query } = useRouter()
+//     // console.log(query.model)
+//     // console.log(mobile.Samsung.model['S20 Ultra'].back)
+//     return (
+//         <>
+//             <Navbar />
+//             <div className=''>
+//                 {
+//                     Object.keys(theme).map((item, index) => {
+//                         return (
 
-                            <div className='grid grid-cols-2  ' key={index}>
-                                {
-                                    theme[item].map((item, index) => {
-                                        return (
-                                            <div key={index + 'jadi'} className=''>
-                                                <Populated image={item.URL} phone={mobile.Samsung?.model['S22 Ultra'].back} skin={mobile.Samsung?.model['S20 Ultra'].skin} />
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </div>
+//                             <div className='grid grid-cols-2  ' key={index}>
+//                                 {
+//                                     theme[item].map((item, index) => {
+//                                         return (
+//                                             <div key={index + 'jadi'} className=''>
+//                                                 <Populated image={item.URL} phone={mobile.Samsung?.model['S22 Ultra'].back} skin={mobile.Samsung?.model['S20 Ultra'].skin} />
+//                                             </div>
+//                                         )
+//                                     })
+//                                 }
+//                             </div>
 
-                        )
-                    })
-                }
-            </div>
-        </>
-    )
-}
+//                         )
+//                     })
+//                 }
+//             </div>
+//         </>
+//     )
+// }
 
-export default index
+// export default index
